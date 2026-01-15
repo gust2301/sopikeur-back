62d1f34428d4d342f73b241509f83097ae50d7f0
AugustinVarore <augustinjrvarore@gmail.com>
2026-01-15 14:21:03 +0100

    initial commit

---
+++ src/main/java/sn/sopikeur/SopikeurBackApplication.java
@@ -0,0 +1,13 @@
+package sn.sopikeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopikeurBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopikeurBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/sopikeur/SopikeurBackApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.sopikeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopikeurBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
